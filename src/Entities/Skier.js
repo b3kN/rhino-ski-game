598b9fbf69c649b289e737057178eefcde8b2afa
Nick Bekeris <nick@b3kn.tech>
2019-09-17 23:57:46 -0500

    Bug fix for app crash after user crashes
    + Implemented updates to set if the skier has crashed and needs to recover
    + Added checks to reset recovery and move skier to safe position
    + Created functionality to allow user to begin route after crashing

--- src/Entities/Skier.js
+++ src/Entities/Skier.js
@@ -4,9 +4,9 @@ import { intersectTwoRects, Rect } from "../Core/Utils";
 
 export class Skier extends Entity {
     assetName = Constants.SKIER_DOWN;
-
     direction = Constants.SKIER_DIRECTIONS.DOWN;
     speed = Constants.SKIER_STARTING_SPEED;
+    recoverCrash = false;
 
     constructor(x, y) {
         super(x, y);
@@ -62,20 +62,38 @@ export class Skier extends Entity {
     }
 
     turnLeft() {
-        if(this.direction === Constants.SKIER_DIRECTIONS.LEFT) {
-            this.moveSkierLeft();
-        }
-        else {
-            this.setDirection(this.direction - 1);
+        // Perform check to see if the skier has crashed
+        if (this.direction === Constants.SKIER_DIRECTIONS.CRASH) {
+            // If skier has crashed, set boolean for recovering skier after crash,
+            // and set the direction to expected left-face direction
+            this.recoverCrash = true;
+            this.setDirection(Constants.SKIER_DIRECTIONS.LEFT);
+        } else {
+            // If the skier has not crashed at this point and direction is set to left-face,
+            // then move the skier left in position.
+            // Otherwise, update set direction one step to left.
+            if (this.direction === Constants.SKIER_DIRECTIONS.LEFT) 
+                this.moveSkierLeft();
+            else
+                this.setDirection(this.direction - 1);
         }
     }
 
     turnRight() {
-        if(this.direction === Constants.SKIER_DIRECTIONS.RIGHT) {
-            this.moveSkierRight();
-        }
-        else {
-            this.setDirection(this.direction + 1);
+        // Perform check to see if the skier has crashed
+        if (this.direction === Constants.SKIER_DIRECTIONS.CRASH) {
+            // If skier has crashed, set boolean for recovering skier after crash,
+            // and set the direction to expected right-face direction
+            this.recoverCrash = true;
+            this.setDirection(Constants.SKIER_DIRECTIONS.RIGHT);
+        } else {
+            // If the skier has not crashed at this point and direction is set to right-face,
+            // then move the skier right in position.
+            // Otherwise, update set direction one step to right.
+            if (this.direction === Constants.SKIER_DIRECTIONS.RIGHT)
+                this.moveSkierRight();
+            else
+                this.setDirection(this.direction + 1);
         }
     }
 
@@ -111,8 +129,18 @@ export class Skier extends Entity {
             return intersectTwoRects(skierBounds, obstacleBounds);
         });
 
-        if(collision) {
-            this.setDirection(Constants.SKIER_DIRECTIONS.CRASH);
+        // If the recover crash boolean is set to false and collision was found,
+        // then set the direction to the crash setting.
+        if (!this.recoverCrash) {
+            if (collision) this.setDirection(Constants.SKIER_DIRECTIONS.CRASH);
+        } else {
+            // If the recover crash boolean is set to true and collision was found,
+            // reset the recover crash boolean to false to allow user input.
+            // Otherwise, move skier down until it has cleared the obstacle.
+            if (!collision)
+                this.recoverCrash = false;
+            else 
+                this.moveSkierDown();
         }
-    };
+    }
 }
