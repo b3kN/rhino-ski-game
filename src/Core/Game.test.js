d772246470ba4a9c532928cc0c0a94dfac2c5400
Nick Bekeris <nick@b3kn.tech>
2019-09-20 11:38:18 -0500

    Updated tests for moving and jumping
    + Added initial set up to match new constants for skier 
    + Added tests for performing moves in left & right directions.
    + Added tests for jumps and jumps collision

--- src/Core/Game.test.js
+++ src/Core/Game.test.js
@@ -15,56 +15,258 @@ describe('Test Game Usage', () => {
     assetManager = new AssetManager();
     skier = new Skier(0, 0);
 
-    const obstacle = new Obstacle(0, 0);
-    obstacle.assetName = Constants.TREE;
-    obstacleManager.obstacles.push(obstacle);
-
     Object.entries(Constants.ASSETS).forEach(function ([assetName]) {
       assetManager.loadedAssets[assetName] = { height: 2, width: 2 };
     });
   });
 
   test('Tree obstacle is loaded', () => {
+    const obstacle = new Obstacle(0, 0);
+    obstacle.assetName = Constants.TREE;
+    obstacleManager.obstacles.push(obstacle);
+
     expect(obstacleManager.obstacles.length).toBeGreaterThanOrEqual(1);
   });
 
   test('Skier is initialized', () => {
-    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.DOWN);
-  });
-
-  test('Skier turns left', () => {
-    skier.turnLeft();
-    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT_DOWN);
-  });
-
-  test('Skier turns full left', () => {
-    skier.turnLeft();
-    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT_DOWN);
-    skier.turnLeft();
     expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT);
   });
 
-  test('Skier turns right', () => {
+  test('Skier moves left', () => {
+    skier.moveSkierLeft();
+    expect(skier.x).toBe(-10);
+  });
+
+  test('Skier turns from left-face to left-down', () => {
+    skier.turnRight();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT_DOWN);
+  });
+
+  test('Skier turns from left-face to down-face', () => {
+    skier.turnRight();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT_DOWN);
+
+    skier.turnRight();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.DOWN);
+  });
+
+
+  test('Skier turns from left-face to down-face and moves', () => {
+    skier.turnRight();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT_DOWN);
+
+    skier.turnRight();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.DOWN);
+
+    skier.moveSkierDown();
+    expect(skier.y).toBe(10);
+  });
+
+  test('Skier turns from left-face to right-down', () => {
+    skier.turnRight();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT_DOWN);
+
+    skier.turnRight();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.DOWN);
+
     skier.turnRight();
     expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT_DOWN);
   });
 
-  test('Skier turns full right', () => {
+  test('Skier turns from left-face to right-face', () => {
+    skier.turnRight();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT_DOWN);
+
+    skier.turnRight();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.DOWN);
+
     skier.turnRight();
     expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT_DOWN);
+
     skier.turnRight();
     expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT);
   });
 
+  test('Skier turns from left-face to right-face and moves', () => {
+    skier.turnRight();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT_DOWN);
+
+    skier.turnRight();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.DOWN);
+
+    skier.turnRight();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT_DOWN);
+
+    skier.turnRight();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT);
+
+    skier.moveSkierRight();
+    expect(skier.x).toBe(10);
+  });
+
+  test('Skier turns from right-face to right-down', () => {
+    skier.setDirection(Constants.SKIER_DIRECTIONS.RIGHT);
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT_DOWN);
+  });
+
+  test('Skier turns from right-face to down-face', () => {
+    skier.setDirection(Constants.SKIER_DIRECTIONS.RIGHT);
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT_DOWN);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.DOWN);
+  });
+
+  test('Skier turns from right-face to down-face and moves', () => {
+    skier.setDirection(Constants.SKIER_DIRECTIONS.RIGHT);
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT_DOWN);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.DOWN);
+
+    skier.moveSkierDown();
+    expect(skier.y).toBe(10);
+  });
+
+  test('Skier turns from right-face to left-down', () => {
+    skier.setDirection(Constants.SKIER_DIRECTIONS.RIGHT);
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT_DOWN);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.DOWN);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT_DOWN);
+  });
+
+  test('Skier turns from right-face to left-face', () => {
+    skier.setDirection(Constants.SKIER_DIRECTIONS.RIGHT);
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT_DOWN);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.DOWN);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT_DOWN);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT);
+  });
+
+  test('Skier turns from right-face to left-face and moves', () => {
+    skier.setDirection(Constants.SKIER_DIRECTIONS.RIGHT);
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT_DOWN);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.DOWN);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT_DOWN);
+
+    skier.turnLeft();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT);
+
+    skier.moveSkierLeft();
+    expect(skier.x).toBe(-10);
+  });
+
+  test('Skier turns from left-face directly to down-face', () => {
+    skier.turnDown();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.DOWN);
+  });
+
   test('Skier has collided', () => {
+    const obstacle = new Obstacle(0, 10);
+    obstacle.assetName = Constants.TREE;
+    obstacleManager.obstacles.push(obstacle);
+    expect(obstacleManager.obstacles.length).toBeGreaterThanOrEqual(1);
+
+    skier.moveSkierDown();
+    expect(skier.y).toBe(10);
+
     skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
     expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.CRASH);
   });
 
   test('Skier turns left from collision', () => {
+    const obstacle = new Obstacle(0, 10);
+    obstacle.assetName = Constants.TREE;
+    obstacleManager.obstacles.push(obstacle);
+    expect(obstacleManager.obstacles.length).toBeGreaterThanOrEqual(1);
+
+    skier.moveSkierDown();
+    expect(skier.y).toBe(10);
+
     skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
     expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.CRASH);
     skier.turnLeft();
     expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.LEFT);
+  });
+
+  test('Skier turns right from collision', () => {
+    const obstacle = new Obstacle(0, 10);
+    obstacle.assetName = Constants.TREE;
+    obstacleManager.obstacles.push(obstacle);
+    expect(obstacleManager.obstacles.length).toBeGreaterThanOrEqual(1);
+
+    skier.moveSkierDown();
+    expect(skier.y).toBe(10);
+
+    skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.CRASH);
+    skier.turnRight();
+    expect(skier.direction).toBe(Constants.SKIER_DIRECTIONS.RIGHT);
+  });
+
+  test('Skier initiates a jump', () => {
+    const obstacle = new Obstacle(0, 10);
+    obstacle.assetName = Constants.SKIER_RAMP;
+    obstacleManager.obstacles.push(obstacle);
+    expect(obstacleManager.obstacles.length).toBeGreaterThanOrEqual(1);
+
+    skier.moveSkierDown();
+    expect(skier.y).toBe(10);
+
+    skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
+    expect(skier.skierIsJumping).toBe(true);
+  });
+
+  test('Skier is jumping and crashes into tree', () => {
+    const ramp = new Obstacle(0, 10);
+    ramp.assetName = Constants.SKIER_RAMP;
+    obstacleManager.obstacles.push(ramp);
+    expect(obstacleManager.obstacles.length).toBeGreaterThanOrEqual(1);
+
+    const tree = new Obstacle(0, 15);
+    tree.assetName = Constants.SKIER_TREE;
+    obstacleManager.obstacles.push(tree);
+    expect(obstacleManager.obstacles.length).toBeGreaterThanOrEqual(2);
+
+    skier.moveSkierDown();
+    expect(skier.y).toBe(10);
+
+    skier.checkIfSkierHitObstacle(obstacleManager, assetManager);
+    expect(skier.skierIsJumping).toBe(true);
+
+    skier.moveSkierDown();
+    expect(skier.y).toBe(15);
   });
 });
