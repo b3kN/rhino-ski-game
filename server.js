c27e0230ecc16a500ab9c934fa825bb2c508e733
Nick Bekeris <nick@b3kn.tech>
2019-09-20 23:43:27 -0500

    Updates to dependencies and server deployment

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
 });
 
 app.listen(port, () => console.log("Started on port " + port));
